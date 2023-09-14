a962a830ad65e3133aea601d95bd13774774e41e
niekkemp <niekkemp2002@gmail.com>
2023-09-15 01:52:06 +0200

    .

--- baseline2.py
+++ baseline2.py
@@ -1,61 +1,67 @@
 # not all imports are used here, we should clean them up before delivering
+from matplotlib import pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from utils import *
 
-# open and read file
-with open('dialog_acts.csv', 'r') as file:
-    lines = file.readlines()
 
-# this is to separate the label and sentence correctly, had some problems on some lines without this step
-data = []
-for line in lines:
-    parts = line.strip().split(' ', 1)
-    if len(parts) == 2:
-        label, sentence = parts
-        data.append((label.lower(), sentence))
+def process(path):
+    # Load saved dataet into df
+    df = pd.DataFrame(load_csv(path), columns=["Label", "Sentence"])
 
-# here the label column is for the type of dialog act and the sentence column for the dialog acts
-df = pd.DataFrame(data, columns=['Label', 'Sentence'])
+    # split the dataset into training 85% and testing 15%
+    X = df["Sentence"]
+    y = df["Label"]
 
-# lowered the sentences but this did not make any difference in the prediction till now
-df['Sentence'] = df['Sentence'].str.lower()
+    # the random state is that the split is done in the same way every time the code is being run
+    # so that we don t have to make separate files for the data splits
+    return train_test_split(X, y, test_size=0.15, random_state=42)
 
-# lower labels
-df['Label'] = df['Label'].str.lower()
 
-# split the dataset into training 85% and testing 15% 
-X = df['Sentence'].astype(str)
-y = df['Label']
+def baseline_classifier2(it):
+    if it.find("food"):
+        return "inform"
+    return "T"
 
-#print(y) - this was just to check that the labels are split correctly
 
-# the random state is that the split is done in the same way every time the code is being run
-# so that we don t have to make separate files for the data splits
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
+# Define paths
+data_path = "./data/dialog_acts.csv"
+data_path_dedup = "./data/dialog_acts_dedup.csv"
 
-# for the second baseline I think it would be best if the rules are written in descending order from the most used label to the least one, so that the chances of a correct classification increases
-#so first we have inform than any other, that should ensure that we start from a 40% accuracy w this classifier too
-def baseline_classifier2(input_text):
-  if input_text.find('i m looking for') or input_text.find('i am looking for') or input_text.find('any type')or input_text.find('moderate price') or input_text.find('restaurant') or input_text.find('i dont care') or input_text.find('spanish') or input_text.find('spanish food') or input_text.find('moderate') :
-    return 'inform'
-    return 'null'
+X_train, X_test, y_train, y_test = process(data_path)
 
 # Test any baseline classifier on the test data
 correct_predictions = 0
 total_predictions = len(y_test)
 
+y_predicted = X_train.apply(lambda x: baseline_classifier2(x))
+
 for sentence, true_label in zip(X_test, y_test):
     # the next 2 lines are for the 2nd classifier
-    predicted_label = baseline_classifier2(sentence) 
+    predicted_label = baseline_classifier2(sentence)
     if predicted_label == true_label:
         correct_predictions += 1
 
-print(f'Accuracy on test data: {correct_predictions / total_predictions}')
+print(f"Accuracy on test data: {correct_predictions / total_predictions}")
+
+
+print(y_train.value_counts(), y_predicted.value_counts())
+
+
+confusion = confusion_matrix(y_train, y_predicted)
+
+
+disp = ConfusionMatrixDisplay(
+    confusion_matrix=confusion,
+    display_labels=set(pd.concat([y_train.drop_duplicates(), pd.Series(["T"])])),
+)
+disp.plot()
+plt.show()
--- classifier1.py
+++ classifier1.py
@@ -4,10 +4,11 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
 
 from utils import *
 
@@ -38,11 +39,18 @@ def train(classifier, X_train, y_train):
     return classifier
 
 
-def evaluate(classifier, X_test, y_test):
+def evaluate(classifier, X_test, y_test, classes):
     X_test = vectorizer.transform(X_test)
     y_predicted = classifier.predict(X_test)
+
     accuracy = accuracy_score(y_test, y_predicted)
     print(f"Accuracy on test data: {accuracy}")
+
+    confusion = confusion_matrix(y_test, y_predicted, labels=classes)
+    print(confusion)
+    disp = ConfusionMatrixDisplay(confusion_matrix=confusion, display_labels=classes)
+    disp.plot()
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -57,10 +65,10 @@ if __name__ == "__main__":
     # classifier =   SVC(kernel="linear", C=0.025)
     classifier = KNeighborsClassifier(3)
 
-    X_train, X_test, y_train, y_test = process(data_path_dedup)
+    X_train, X_test, y_train, y_test = process(data_path)
 
     classifier = train(classifier, X_train, y_train)
-    evaluate(classifier, X_test, y_test)
+    evaluate(classifier, X_test, y_test, classifier.classes_)
 
     # Start diaglogue
     while True:
